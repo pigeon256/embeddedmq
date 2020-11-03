88cf86f5e5765f53e813dd7fabcb58963e5314e3
sarpadhy <er.s.s.padhy@gmail.com>
2020-11-03 15:51:48 +0530

    added

---
+++ src/main/java/com/example/demo/IbmmqApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmmqApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(IbmmqApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/demo/IbmmqApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IbmmqApplicationTests {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	@Test
+	public void testReceive() throws Exception {
+		this.jmsTemplate.convertAndSend("helloworld.q", "Hello, world!");
+		this.jmsTemplate.convertAndSend("helloworld.topic2", "testdemo");
+
+		this.jmsTemplate.setReceiveTimeout(10_000);
+		assertThat(this.jmsTemplate.receiveAndConvert("helloworld.q").toString()).isEqualTo("Hello, world!");
+	}
+
+}
